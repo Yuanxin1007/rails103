a4e7ea69c787aeb2f88669c15aecf7b56677aeec
Liu Yuan Xin <Yuanxin1007@gmail.com>
2017-08-12 22:58:43 +0800

    implement is_member_of?(group)

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,10 @@ class User < ApplicationRecord
          has_many :groups
          has_many :posts
 
-        
+def is_member_of?(group)
+  participated_groups.include?(group)
+end
+
            has_many :group_relationships
            has_many :participated_groups, :through => :group_relationships, :source => :group
 end
